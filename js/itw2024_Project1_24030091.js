c9cf856816ccee40dd6a165112022705a89d4e0a
AdrielReihansyah <adrielreihansyah05@gmail.com>
2025-01-07 16:57:13 +0700

    done

---
+++ js/itw2024_Project1_24030091.js
@@ -0,0 +1,19 @@
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form')
+      contactMessage = document.getElemenntById('contact-message')
+      
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publickey
+    emailjs.sendForm
+}
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
